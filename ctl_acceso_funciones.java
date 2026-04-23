512a26816ca0061b6438621bab7eb7f5a20d8fac
Pipe <pcgamervc323@gmail.com>
2026-04-22 20:01:02 -0500

    Control de acceso con Funciones

---
+++ ctl_acceso_funciones.java
@@ -0,0 +1,26 @@
+package U;
+
+import java.util.Scanner;
+
+public class ctl_acceso_funciones {
+    public static void main(String[] args) {
+     int edad = getedadbyconsole();
+     validateedad(edad);
+    }
+    public static int getedadbyconsole(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su edad: ");
+        return sc.nextInt();
+    }
+    public static void validateedad(int edad){
+    if (edad > 60) {
+    System.out.println("Acceso preferencial");
+    }   
+    else if (edad >= 18) {
+    System.out.println("Acceso permitido");
+    } 
+    else {
+    System.out.println("Acceso denegado");
+    }
+    }
+}
